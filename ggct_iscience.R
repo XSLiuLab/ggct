e672d460d5e809340a805dded2d1bc734585323f
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-06-11 15:05:55 +0800

    基本完成的reviewer分析问题

--- ggct_iscience.R
+++ ggct_iscience.R
@@ -213,3 +213,65 @@ ggerrorplot(ggct_on_stages, x="classes", y="cnv",
 # does GGCT mRNA and CNV levels increase when the dataset is stratified for
 # RAS mutant cancers and does this have an effect on survival in either cancer stage? 
 # The authors should do this analysis and show that data.
+library(dplyr)
+
+load(file = "tcga_ggct.RData")
+load(file="kras_status.RData")
+table(substr(kras_status$barcodes, 14, 15))
+load("./ggct_survival_data_on_metastasis_and_stages.Rdata")
+
+df  = left_join(kras_status, TCGA_Clinical[, c("sampleID", "OS", "OS.time",
+                                               "pathologic_stage")], by = c("barcodes"="sampleID"))
+df = left_join(df, df.ggct, by = c("barcodes"="sampleID"))
+
+df = df %>% filter(!is.na(expr), !is.na(copynumber))
+df = df %>% 
+  mutate(classes = ifelse(pathologic_stage %in% c("Stage I", "Stage IA", "Stage IB"), "Early", 
+                ifelse(pathologic_stage %in% c("Stage IIIA", "Stage IIIB", "Stage IV"), "Late", NA)))
+df_long = df %>% 
+  tidyr::gather("type", "value", expr, copynumber)
+
+library(ggpubr)
+p1 = ggboxplot(df, x="status", y = "copynumber", 
+          xlab = "KRAS mutation status", ylab = "GGCT copy number ratio (log2 based)") + 
+  stat_compare_means(comparisons = list(c("0", "1")))
+p2 = ggboxplot(df, x="status", y = "expr", 
+          xlab = "KRAS mutation status", ylab = "GGCT expression (log2 based)") + 
+  stat_compare_means(comparisons = list(c("0", "1")))
+
+cowplot::plot_grid(p1, p2)
+
+library(survival)
+library(survminer)
+
+coxph(Surv(OS.time, OS) ~ expr, data = df)
+coxph(Surv(OS.time, OS) ~ copynumber, data = df)
+coxph(Surv(OS.time, OS) ~ expr + strata(status), data = df)
+coxph(Surv(OS.time, OS) ~ copynumber + strata(status), data = df)
+
+coxph(Surv(OS.time, OS) ~ expr, data = subset(df, classes=="Early"))
+coxph(Surv(OS.time, OS) ~ copynumber, data = subset(df, classes=="Early"))
+coxph(Surv(OS.time, OS) ~ expr + strata(status), data = subset(df, classes=="Early"))
+coxph(Surv(OS.time, OS) ~ copynumber + strata(status), data = subset(df, classes=="Early"))
+
+coxph(Surv(OS.time, OS) ~ expr, data = subset(df, classes=="Late"))
+coxph(Surv(OS.time, OS) ~ copynumber, data = subset(df, classes=="Late"))
+coxph(Surv(OS.time, OS) ~ expr + strata(status), data = subset(df, classes=="Late"))
+coxph(Surv(OS.time, OS) ~ copynumber + strata(status), data = subset(df, classes=="Late"))
+
+th = 0.20
+df2 = df %>% 
+  mutate(cp_status = ifelse(copynumber>0.4, "GGCT amplification", 
+                            ifelse(copynumber<0.1&copynumber>-0.1, "GGCT normal", NA_character_)),
+         expr_status = ifelse(expr < quantile(expr, th), "GGCT low expression",
+                              ifelse(expr > quantile(expr, 1-th), "GGCT high expression", NA_character_))) %>% 
+  filter(!is.na(classes), OS.time<3000) %>% 
+  rename(KRAS_Status=status, Stage=classes) %>% 
+  mutate(KRAS_Status = ifelse(KRAS_Status==1, "Mut", ifelse(KRAS_Status==0, "WT", NA_character_)))
+
+
+fit = survfit(Surv(OS.time, OS) ~ expr_status, data = df2)
+ggsurvplot_facet(fit, df2, pval = TRUE, facet.by = c("Stage", "KRAS_Status"), palette = "jco")
+fit = survfit(Surv(OS.time, OS) ~ cp_status, data = df2)
+ggsurvplot_facet(fit, df2, pval = TRUE, facet.by = c("Stage", "KRAS_Status"), palette = "jco")
+
